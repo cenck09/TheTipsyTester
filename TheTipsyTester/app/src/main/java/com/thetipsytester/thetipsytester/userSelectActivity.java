f523853710d751a1eeefe0fff8d210f3ff393f02
amh6252 <amhartman8@verizon.net>
2016-03-30 13:43:06 -0400

    App screen updates 3/30

--- TheTipsyTester/app/src/main/java/com/thetipsytester/thetipsytester/userSelectActivity.java
+++ TheTipsyTester/app/src/main/java/com/thetipsytester/thetipsytester/userSelectActivity.java
@@ -1,7 +1,9 @@
 package com.thetipsytester.thetipsytester;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class userSelectActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class userSelectActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_select);
+    }
+    public void addUser(View view){
+        //When the + button is clicked from user selection page
+        Intent intent = new Intent(this, newUserActivity.class);
+
+        startActivity(intent);
     }
 }
