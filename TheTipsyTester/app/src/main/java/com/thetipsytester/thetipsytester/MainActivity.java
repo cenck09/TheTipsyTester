ead76e6a4295c2becc5730e37ac2c9cdd06a9d37
nt10307 <nt10307@gmail.com>
2016-04-07 13:28:58 -0400

    Shared Preferences
    Not working properly.  Going to talk to Blum

--- TheTipsyTester/app/src/main/java/com/thetipsytester/thetipsytester/MainActivity.java
+++ TheTipsyTester/app/src/main/java/com/thetipsytester/thetipsytester/MainActivity.java
@@ -1,23 +1,33 @@
 package com.thetipsytester.thetipsytester;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
         setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+        View view = this.getWindow().getDecorView();
+        view.setBackgroundColor(Integer.parseInt(sharedPref.getString("color", "-1")));
 
     }
+
+
+
 
 
 
@@ -62,7 +72,5 @@ public class MainActivity extends AppCompatActivity {
 
         startActivity(intent);
     }
-
-
 
 }
