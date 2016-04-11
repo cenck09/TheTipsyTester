86ec4f243f43369c77a1c2133c66003c787b2bf9
nt10307 <nt10307@gmail.com>
2016-04-11 14:25:45 -0400

    Edits to Balance Test

--- TheTipsyTester/app/src/main/java/com/thetipsytester/thetipsytester/balanceTest.java
+++ TheTipsyTester/app/src/main/java/com/thetipsytester/thetipsytester/balanceTest.java
@@ -1,12 +1,14 @@
 package com.thetipsytester.thetipsytester;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.view.View;
+import android.os.CountDownTimer;
 import android.widget.TextView;
 
 /**
@@ -17,9 +19,17 @@ public class balanceTest extends Activity implements SensorEventListener{
 
     Sensor accelerometer;
     SensorManager sm;
+
     TextView acceleration;
-    boolean measuring = false;
+    TextView timerText;
+
+    int finalScore;
+    boolean measuring = false, waiting = false;
     float totalAcceleration = 0, changeAcceleration = 0, lastTotal = 0, x, y ,z;
+    final long startTime = 10000, pauseTime = 3000, interval=1000;
+
+    CountDownTimer countDownTimer = new MyCountDownTimer(startTime, interval);
+    CountDownTimer pauseTimer = new MyCountDownTimer2(pauseTime, interval);
 
 
     @Override
@@ -32,7 +42,62 @@ public class balanceTest extends Activity implements SensorEventListener{
         sm.registerListener(this,accelerometer,SensorManager.SENSOR_DELAY_NORMAL);
 
         acceleration = (TextView)findViewById(R.id.acceleration);
+        timerText = (TextView)findViewById(R.id.balanceCountDown);
 
+        timerText.setText(timerText.getText()+String.valueOf(startTime / 1000));
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setTitle("Balance Test");
+        alertDialogBuilder.setMessage("To perform this test, stand on one leg and hold your arms out to your sides, phone in hand.\n\nHold this position for 10 seconds while the phone measures your balance.\n\nYou will have 3 seconds after clicking 'Start' to get into position.");
+        alertDialogBuilder.setCancelable(false)
+                .setPositiveButton("Start", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        waiting = true;
+                        measure();
+                    }
+                });
+
+        // create alert dialog
+        AlertDialog alertDialog = alertDialogBuilder.create();
+
+        // show it
+        alertDialog.show();
+    }
+
+    public class MyCountDownTimer extends CountDownTimer{
+        public MyCountDownTimer(long startTime, long interval){
+            super(startTime, interval);
+        }
+
+        @Override
+        public void onFinish(){
+            measuring = false;
+            lastTotal = totalAcceleration;
+            totalAcceleration = 0;
+            finalScore = (int)lastTotal;
+            timerText.setText("Final Score: " + (int)lastTotal);
+        }
+
+        @Override
+        public void onTick(long millisUntilFinished){
+            timerText.setText("Time: " + millisUntilFinished/1000);
+        }
+    }
+
+    public class MyCountDownTimer2 extends CountDownTimer{
+        public MyCountDownTimer2(long startTime, long interval){
+            super(startTime, interval);
+        }
+        @Override
+        public void onFinish(){
+            countDownTimer.start();
+            measuring = true;
+            waiting = false;
+        }
+        @Override
+        public void onTick(long millisUntilFinished){
+            timerText.setText("Starts in: " + millisUntilFinished/1000);
+        }
     }
 
     @Override
@@ -44,22 +109,12 @@ public class balanceTest extends Activity implements SensorEventListener{
         if (measuring) {
             totalAcceleration = totalAcceleration + changeAcceleration;
         }
-        acceleration.setText("X: " + x +
-                            "\nY: " + y +
-                            "\nZ: " + z +
-                            "\nchange: " + changeAcceleration +
-                            "\ntotal: " + totalAcceleration +
-                            "\nlast: " + lastTotal);
-
+        acceleration.setText("measurements: " + totalAcceleration +
+                            "\ntotal: " + lastTotal);
     }
 
-
-    public void measure(View view){
-        if(measuring){
-            lastTotal = totalAcceleration;
-            totalAcceleration = 0;
-        }
-        measuring = !measuring;
+    public void measure(){
+        pauseTimer.start();
     }
 
     @Override
